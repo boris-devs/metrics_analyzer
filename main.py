122ec0bd4c848d8fe3a375fb3fd4099ddfabf6ba
xborismenx <boris.tikhonov.21@gmail.com>
2026-04-30 22:06:20 +0300

    Enhance `main.py` to improve report type handling and add error handling for invalid input and file operations

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import argparse
 from reports.clickbait import ClickbaitReportCsv
 
+
 def main():
 	parser = argparse.ArgumentParser(description="YouTube Metrics Processor.")
 	parser.add_argument("--files", required=True, nargs="+", help="list of files to be processed")
 	parser.add_argument("--report", required=True, help="type of report to generate")
 
 	args = parser.parse_args()
+
 	files = args.files
 	type_report = args.report
-	if type_report == "clickbait":
-		clickbait = ClickbaitReportCsv(files)
-		print(clickbait.generate_report())
+
+	reports_map = {"clickbait": ClickbaitReportCsv}
+	report_class = reports_map.get(type_report, None)
+	if report_class is None:
+		print("Invalid report type")
+
+	try:
+		report_instance = report_class(files)
+		print(report_instance.generate_report())
+	except FileNotFoundError as e:
+		print(f"File Error: {e}")
+	except Exception as e:
+		print(f"Something went wrong during parsing: {e}")
 
 
 if __name__ == "__main__":
