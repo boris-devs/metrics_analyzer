8ff67e18ec2d4faab798ba326fb2f74b8867a2ba
xborismenx <boris.tikhonov.21@gmail.com>
2026-04-30 21:58:53 +0300

    Refactor `ClickbaitReportCsv` to inherit common functionality from new `BaseReportCsv` class

---
+++ reports/base_report.py
@@ -0,0 +1,44 @@
+import csv
+from abc import ABC, abstractmethod
+
+from tabulate import tabulate
+
+from settings import METRICS_FILES_PATH
+
+
+class BaseReportCsv(ABC):
+	def __init__(self, files_path: list):
+		self.files_path = files_path
+
+	def exist_file(self, file: str):
+		full_path_file = METRICS_FILES_PATH.joinpath(file)
+		if not full_path_file.exists():
+			raise FileNotFoundError(f"File {file} not found")
+		return full_path_file
+
+	def processing_file(self, file: str):
+		processed_rows = []
+		full_path_file = self.exist_file(file)
+		with full_path_file.open("r", encoding="utf-8") as f:
+			csv_file = csv.DictReader(f)
+			for row in csv_file:
+				filtered_data = self.filter_logic(row)
+				if filtered_data:
+					processed_rows.append(filtered_data)
+		return processed_rows
+
+	def generate_report(self):
+		all_report_data = []
+		for file in self.files_path:
+			all_report_data.extend(self.processing_file(file))
+
+		all_report_data.sort(key=lambda x: x['ctr'], reverse=True)
+		return self.grid_format_report(all_report_data)
+
+	def grid_format_report(self, data: list[dict]):
+		return tabulate(data, headers="keys", tablefmt="grid")
+
+
+	@abstractmethod
+	def filter_logic(self, row: dict):
+		pass
--- reports/clickbait.py
+++ reports/clickbait.py
@@ -1,44 +1,9 @@
-import csv
-
-from tabulate import tabulate
-
-from settings import METRICS_FILES_PATH
+from reports.base_report import BaseReportCsv
 
 
-class ClickbaitReportCsv:
-	def __init__(self, files_path: list):
-		self.files_path = files_path
+class ClickbaitReportCsv(BaseReportCsv):
 
-	def exist_file(self, file: str):
-		full_path_file = METRICS_FILES_PATH.joinpath(file)
-		if not full_path_file.exists():
-			raise FileNotFoundError(f"File {file} not found")
-		return full_path_file
-
-	def generate_report(self):
-		all_report_data = []
-		for file in self.files_path:
-			all_report_data.extend(self.processing_file(file))
-
-		all_report_data.sort(key=lambda x: x['ctr'], reverse=True)
-		return self.grid_format_report(all_report_data)
-
-	def grid_format_report(self, data: list[dict]):
-		return tabulate(data, headers="keys", tablefmt="grid")
-
-	def processing_file(self, file: str):
-		processed_rows = []
-		full_path_file = self.exist_file(file)
-
-		with full_path_file.open("r", encoding="utf-8") as f:
-			csv_file = csv.DictReader(f)
-			for row in csv_file:
-				filtered_data = self.filter_by_ctr_and_retention(row)
-				if filtered_data:
-					processed_rows.append(filtered_data)
-		return processed_rows
-
-	def filter_by_ctr_and_retention(self, row: dict):
+	def filter_logic(self, row: dict):
 		try:
 			ctr = float(row.get("ctr", 0))
 			retention = float(row.get("retention_rate", 0))
